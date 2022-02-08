fbf811a86ad1a3e88b5764e8bb5010d6518f1d75
Ubuntu <azureuser@azubu.2q2netrib1mevjft2iryq40zia.gx.internal.cloudapp.net>
2022-02-08 07:56:08 +0000

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { SafeAreaView, ScrollView, View, Text, Button } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// Import agent from setup
+import { agent } from './src/veramo/setup'
+
+interface Identifier {
+  did: string
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  const [identifiers, setIdentifiers] = useState<Identifier[]>([])
+
+  // Add the new identifier to state
+  const createIdentifier = async () => {
+    const _id = await agent.didManagerCreate()
+    setIdentifiers((s) => s.concat([_id]))
+  }
+
+  // Check for existing identifers on load and set them to state
+  useEffect(() => {
+    const getIdentifiers = async () => {
+      const _ids = await agent.didManagerFind()
+      setIdentifiers(_ids)
+
+      // Inspect the id object in your debug tool
+      console.log('_ids:', _ids)
+    }
+
+    getIdentifiers()
+  }, [])
+
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View style={{ padding: 20 }}>
+          <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Identifiers</Text>
+          <View style={{ marginBottom: 50, marginTop: 20 }}>
+            {identifiers && identifiers.length > 0 ? (
+              identifiers.map((id: Identifier) => (
+                <View key={id.did}>
+                  <Text>{id.did}</Text>
+                </View>
+              ))
+            ) : (
+              <Text>No identifiers created yet</Text>
+            )}
+          </View>
+          <Button onPress={() => createIdentifier()} title={'Create Identifier'} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default App
